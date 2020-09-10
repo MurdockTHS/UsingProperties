7eefe40bab78ec7516b3de40cb28e76155f2002e
Angel Acosta <Mrnurdee@gmail.com>
2020-09-09 17:38:47 -0700

    Add project files.

---
+++ UsingProperties/Animals.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UsingProperties
+{
+    class Animals
+    {
+        public string Name;
+        public string Type;
+        public string Sound;
+        public string Consume;
+        public string Produce;
+        public string Located;
+        private string size;
+
+        public Animals(string aName, string aType, string aSound, string aConsume, string aProduce, string aLocated, string aSize)
+        {
+            Name = aName;
+            Type = aType;
+            Sound = aSound;
+            Consume = aConsume;
+            Produce = aProduce;
+            Located = aLocated;
+            Size = aSize;
+        }
+
+        public string Size
+        {
+            get { return size; }
+            set {
+                if (value == "tiny" || value == "small" || value == "medium" || value == "big")
+                {
+                    size = value;
+                }
+                else
+                {
+                    size = "Invalid size";
+                }
+                
+            }
+        }
+    }
+}
---
+++ UsingProperties/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UsingProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animals Horse = new Animals("Mr.Ed", "horse","neigh","hay", "transportation","back of the farm", "medium");
+            //purposely put the invalid size to demonstates the private getter and setter function
+            Animals Cow = new Animals("Milkshake", "cow", "Moo", "grass", "beef", "backyard", "Mr.Bean");
+            Console.WriteLine(Horse.Size);
+            //should out put a invalid size string
+            Console.WriteLine(Cow.Size);
+        }
+    }
+}
